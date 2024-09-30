86e826cbcd084c3e854a0e360aceeba651e5b137
Luka Govedic <luka.govedic@gmail.com>
2024-09-30 13:34:08 -0400

    Remove duplicate symbols

--- runtime/cilk_abi_cilk_for_serial.c
+++ runtime/cilk_abi_cilk_for_serial.c
@@ -6,6 +6,9 @@
 #include <stdint.h>
 #include "cilk_abi_cilk_for.h"
 
+// CMake counts on the for_loop file to pull in the inlined functions
+#include "cilk2c_inlined.c"
+
 static void cilk_for_impl_64(__cilk_abi_f64_t body, void *data, uint64_t count, unsigned int grain, int inclusive) {
     body(data, 0, count);
 }
